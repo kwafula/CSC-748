0b9e019e530488cbc2b7c119b6ee9c96c4405a1d
Kevin Wafula <95890992+kwafula@users.noreply.github.com>
2023-09-03 18:14:39 -0400

    Update Lab1-1.py

--- Homework-01/Lab1-1.py
+++ Homework-01/Lab1-1.py
@@ -1,1 +1,24 @@
+from pwn import *
+
+# connect to local binary process
+# p = processs("./lab1-1.bin")
+
+# connect to remote server process at port 7011
+p = remote("csc748.hostbin.org", 7011)
+
+# read data upto secret string
+p.recvuntil(b"\n\n")
+
+# get and print secret string 
+s = p.recev(15)
+print(f"The secret string was: {s}")
+
+# send back secret string
+p.sendline(s)
+
+# establish interactive session 
+p.interactive()
+
+
+
 
