fb8f52bf73f59b65a5a9c3a99332febc5e322a77
Kevin Wafula <95890992+kwafula@users.noreply.github.com>
2023-09-03 20:14:54 -0400

    Update lab1-2.c

--- Homework-01/lab1-2.c
+++ Homework-01/lab1-2.c
@@ -1,1 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <signal.h>
 
+void sig_handler(int signum) {
+
+	printf("Timeout\n");
+	exit(0);
+
+}
+
+void init() {
+
+	alarm(60);
+	signal(SIGALRM, sig_handler);
+
+	setvbuf(stdin, NULL, _IONBF, 0);
+	setvbuf(stdout, NULL, _IONBF, 0);
+	setvbuf(stderr, NULL, _IONBF, 0);
+
+	chdir(getenv("HOME"));
+
+}
+
+void win() {
+
+	alarm(0);
+	execl("/bin/bash", "/bin/bash", NULL);
+
+}
+
+int main() {
+
+	long ptr;
+
+	init();
+
+	printf("Hello! Give me a pointer, (as a decimal number), and I'll call it. :)\n");
+
+	scanf("%ld", &ptr);
+
+	((void(*)(void)) ptr)();
+
+	return 0;
+
+}
